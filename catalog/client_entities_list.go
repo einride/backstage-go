9cf139498f87156d3df51ede56baa8649e941225
Oscar Söderlund <odsod@users.noreply.github.com>
2023-03-27 20:10:53 +0200

    feat(catalog): set list entities request query

--- catalog/client_entities_list.go
+++ catalog/client_entities_list.go
@@ -5,10 +5,15 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 // ListEntitiesRequest is the request to the [Client.ListEntities] method.
 type ListEntitiesRequest struct {
+	// Filter for selecting only a subset of all entities.
+	Filter string
+	// Fields for selecting only parts of the full data structure of each entity.
+	Fields string
 	// Offset for pagination.
 	Offset int64
 	// Limit for pagination.
@@ -26,10 +31,24 @@ type ListEntitiesResponse struct {
 // ListEntities lists entities in the catalog.
 //
 // See: https://backstage.io/docs/features/software-catalog/software-catalog-api/#get-entities
-func (c *Client) ListEntities(ctx context.Context, _ *ListEntitiesRequest) (*ListEntitiesResponse, error) {
+func (c *Client) ListEntities(ctx context.Context, request *ListEntitiesRequest) (*ListEntitiesResponse, error) {
 	const path = "/api/catalog/entities"
-	// TODO: Set request query parameters.
 	query := make(url.Values)
+	if request.Offset > 0 {
+		query.Set("offset", strconv.FormatInt(request.Offset, 10))
+	}
+	if request.Limit > 0 {
+		query.Set("limit", strconv.FormatInt(request.Limit, 10))
+	}
+	if request.Filter != "" {
+		query.Set("filter", request.Filter)
+	}
+	if request.Fields != "" {
+		query.Set("fields", request.Fields)
+	}
+	if request.After != "" {
+		query.Set("after", request.After)
+	}
 	var rawEntities []json.RawMessage
 	if err := c.get(ctx, path, query, func(response *http.Response) error {
 		return json.NewDecoder(response.Body).Decode(&rawEntities)
